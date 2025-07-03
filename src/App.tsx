e2fbd0d34d1efb233a05ba28f6daae3c36a37cc9
Morenodev085 <moreno.ferreira11@gmail.com>
2025-07-03 14:45:59 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1>Niteroi em Cena</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/styled.ts
@@ -0,0 +1,24 @@
+import styled, { createGlobalStyle } from "styled-components"
+
+export const color = {
+creme: '#f9f9db',
+preto: '#000',
+Branco: '#fff'
+}
+
+
+export const EstiloGlobal = createGlobalStyle`
+
+
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif
+}
+
+body{
+    background-color: ${color.creme};
+}
+`
